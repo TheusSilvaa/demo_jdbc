23149e2da2e45ebc56ab47c8c353e9156f7905c3
Matheus Silva <msilvarodrigues011@gmail.com>
2026-05-17 00:06:31 -0300

    Resuing instantiation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -48,18 +48,9 @@ public class SellerDaoJDBC implements SellerDao {
 
             st.setInt(1, id);
             rs = st.executeQuery();
-
             if (rs.next()) {
-                Department dep = new Department(); // Linha 53
-                dep.setId(rs.getInt("DepartmentId")); // Linha 54
-                dep.setName(rs.getString("DepName")); // Linha 55
-                Seller obj = new Seller();
-                obj.setId(rs.getInt("Id"));
-                obj.setName(rs.getString("Name"));
-                obj.setEmail(rs.getString("Email"));
-                obj.setBaseSalary(rs.getDouble("BaseSalary"));
-                obj.setBirthDate(rs.getDate("BirthDate"));
-                obj.setDepartment(dep);
+                Department dep = instantiateDepartment(rs);
+                Seller obj = instantiateSeller(rs, dep);
                 return obj;
             }
             return null;
@@ -73,9 +64,15 @@ public class SellerDaoJDBC implements SellerDao {
         }
     }
 
-    @Override
-    public List<Seller> findAll() {
-        return List.of();
+    private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+        Seller obj = new Seller();
+        obj.setId(rs.getInt("Id"));
+        obj.setName(rs.getString("Name"));
+        obj.setEmail(rs.getString("Email"));
+        obj.setBaseSalary(rs.getDouble("BaseSalary"));
+        obj.setBirthDate(rs.getDate("BirthDate"));
+        obj.setDepartment(dep);
+        return obj;
     }
 
     private Department instantiateDepartment(ResultSet rs) throws SQLException {
@@ -84,4 +81,10 @@ public class SellerDaoJDBC implements SellerDao {
         dep.setName(rs.getString("DepName"));
         return dep;
     }
+
+    @Override
+    public List<Seller> findAll() {
+        return List.of();
+    }
+
 }
