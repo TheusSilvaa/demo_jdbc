f1c7d434c61fd68bf73d0db4c58abe5e40253030
Matheus Silva <msilvarodrigues011@gmail.com>
2026-05-17 02:29:58 -0300

    Implementation of findById, findAll, insert, update, delete in department

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -95,7 +95,7 @@ public class SellerDaoJDBC implements SellerDao {
             st.setInt(1, id);
 
             st.executeUpdate();
-            
+
         }
         catch (SQLException e) {
             throw new DbException(e.getMessage());
